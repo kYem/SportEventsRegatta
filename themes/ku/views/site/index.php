465780d4a5e30d5c3ef981bc83aad296571d24a2
kYem <mr.kyem@gmail.com>
2014-03-31 07:09:46 +0100

    Updated front-page margins

--- themes/ku/views/site/index.php
+++ themes/ku/views/site/index.php
@@ -7,18 +7,18 @@ $this->pageTitle=Yii::app()->name;
 <h2>Regatta Summer 2014</h2>
 
 <div>
-<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/header-front.png" alt="header photo" style="float: right">
+<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/header-front.png" alt="header photo" style="float: right; margin: 0 10px">
 
 
 
 
 <p>A regatta is a series of boat races. The term typically describes racing events of rowed or sailed water craft, although some powerboat race series are also called regattas. A regatta often includes social and promotional activities which surround the racing event, and except in the case of boat type (or "class") championships, is usually named for the town or venue where the event takes place..</p>
-<p>Although regattas are typically amateur competitions, they are usually formally structured events, with comprehensive rules describing the schedule and procedures of the event. Regattas may be organized as championships for a particular area or type of boat, but are often held just for the joy of competition, camaraderie, and general promotion of the sport.</p><p>
+<p>Although regattas are typically amateur competitions, they are usually formally structured events, with comprehensive rules describing the schedule and procedures of the event. </p>
 </div>
 <br><br>
 <div>
 <h3>Compete and Win</h3>
-<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/winners.png" alt="header photo" style="float: right">
+<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/winners.png" alt="header photo" style="float: right; margin: 15px;">
 <p>Sailing race events are typically held for a single class (a single model of boat, such as the Islander 36) and usually last more than one day. Regattas may be hosted by a yacht club, sailing association, town or school as in the case of the UK's National School Sailing Association and Interscholastic Sailing Association
 </p>
 <p>One of the largest and most popular rowing regattas is the Henley Royal Regatta held on the River Thames, England. One of the largest and oldest yachting regattas in the world is Cowes Week, which is held annually by the Royal Yacht Squadron in Cowes, England, and usually attracts over 900 sailboats. Cowes Week is predated by the Cumberland Cup (1775), Port of Dartmouth Royal Regatta (1822) and Port of Plymouth Regatta (1823). North America's oldest regatta is the Royal St. John's Regatta held on Quidi Vidi Lake in St. John's, Newfoundland every year since 1826. Or so it say</p>
